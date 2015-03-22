47c0deaa99356a1d4e9521309ffdf832ab816932
andze132 <azeijlon@live.se>
2015-03-22 16:48:39 +0100

    first inskick

--- src/se/liu/ida/andze132/tddd78/javaproject/TestClass.java
+++ src/se/liu/ida/andze132/tddd78/javaproject/TestClass.java
@@ -1,7 +1,6 @@
 package se.liu.ida.andze132.tddd78.javaproject;
 
 public class TestClass {
-    private final static int INTERVAL = 100;
 
     public static void main(String[] args) {
         GRID grid = new GRID(1);
