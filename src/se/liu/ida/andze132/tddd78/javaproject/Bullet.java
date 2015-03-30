5a6ebc212951d4741bd22fabdf82fa5168ca4e7e
Oile995 <oscar_hillbo@hotmail.com>
2015-03-31 00:05:40 +0200

    kuk

---
+++ src/se/liu/ida/andze132/tddd78/javaproject/Bullet.java
@@ -0,0 +1,26 @@
+package se.liu.ida.andze132.tddd78.javaproject;
+
+/**
+ * Created by Administratör on 2015-03-30.
+ */
+public class Bullet
+{
+    private float x;
+    private float y;
+    private float velocity;
+    private int targetX;
+    private int targetY;
+
+    public Bullet(final float x, final float y, final float velocity, final int targetX, final int targetY) {
+	this.x = x;
+	this.y = y;
+	this.velocity = velocity;
+	this.targetX = targetX;
+	this.targetY = targetY;
+    }
+
+    public void updateBullet(){
+
+        }
+
+}
---
+++ src/se/liu/ida/andze132/tddd78/javaproject/NormalBullet.java
@@ -0,0 +1,14 @@
+package se.liu.ida.andze132.tddd78.javaproject;
+
+import java.util.Vector;
+
+/**
+ * Created by Administratör on 2015-03-30.
+ */
+public class NormalBullet extends Bullet
+{
+    public NormalBullet(final float x, final float y, final float velocity, final int targetX, final int targetY) {
+	super(x, y, velocity, targetX, targetY);
+    }
+    
+}
